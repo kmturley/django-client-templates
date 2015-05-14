8c3300d9ab9750191289e2f7f8b766f4b59d2089
Kim T <kim@bynd.com>
2015-05-14 18:00:52 -0400

    Load data from server
    use HTTP_ACCEPT to detect request type

--- pages/static/example.js
+++ pages/static/example.js
@@ -8,22 +8,74 @@
 
 (function () {
     'use strict';
-
-    var el = document.getElementById('list'),
-        tmpl = document.getElementById('tmpl-list'),
-        template = new plate.Template(tmpl.text),
-        json = {
-            "people": [
-                {"name": "New data 1"},
-                {"name": "New data 2"},
-                {"name": "New data 3"}
-            ]
-        };
-
-    template.render(json, function (err, data) {
-        el.innerHTML = data;
-    });
+    
+    var module = {
+        /**
+         * @method init
+         */
+        init: function () {
+            var me = this,
+                i = 0;
+            this.list = this.checkElements();
+            
+            // wait one second then load dynamic data
+            window.setTimeout(function () {
+                me.get(window.location.href, function (data) {
+                    for (i = 0; i < me.list.length; i += 1) {
+                        console.log('list', i, me.list[i], data);
+                        me.update(me.list[i], data);
+                    }
+                });
+            }, 1000);
+        },
+        /**
+         * @method checkElements
+         */
+        checkElements: function () {
+            var i = 0,
+                list = [],
+                attr = '',
+                el = null,
+                tmpl = '',
+                divs = document.getElementsByTagName('div');
+            for (i = 0; i < divs.length; i += 1) {
+                attr = divs[i].getAttribute('data-tmpl');
+                if (attr) {
+                    el = document.getElementById(attr);
+                    if (el) {
+                        tmpl = new plate.Template(el.text);
+                        list.push({tmpl: tmpl, el: divs[i]});
+                    }
+                }
+            }
+            return list;
+        },
+        /**
+         * @method update
+         */
+        update: function (item, data) {
+            item.tmpl.render(data, function (err, data) {
+                item.el.innerHTML = data;
+            });
+        },
+        /**
+         * @method get
+         */
+        get: function (url, callback) {
+            console.log('get', url);
+            var me = this;
+            if (this.request) {
+                this.request.abort();
+            } else {
+                this.request = new XMLHttpRequest();
+            }
+            this.request.open('GET', url, true);
+            this.request.setRequestHeader('Accept', 'application/json');
+            this.request.onload = function () {
+                callback(JSON.parse(me.request.response));
+            };
+            this.request.send();
+        }
+    };
+    module.init();
 }());
-
-
-
--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext, loader
 
 def index(request):
-    template = loader.get_template('index.html')
-    context = RequestContext(request, {
-        'people': [
-            {"name": "John Doe"},
-            {"name": "Sally Taylor"},
-            {"name": "David Smith"}
-        ],
-    })
-    return HttpResponse(template.render(context))
+    if request.META.get('HTTP_ACCEPT') == 'application/json':
+        data = {
+            "items": [
+                {"name": "Dynamic data 1", "desc": "Dynamic Lorem ipsum dolor sit amet, consectetur adipiscing elit. 1" },
+                {"name": "Dynamic data 2", "desc": "Dynamic Lorem ipsum dolor sit amet, consectetur adipiscing elit. 2" },
+                {"name": "Dynamic data 3", "desc": "Dynamic Lorem ipsum dolor sit amet, consectetur adipiscing elit. 3"}
+            ]
+        }
+        return JsonResponse(data)
+    else:
+        template = loader.get_template('index.html')
+        context = RequestContext(request, {
+            'items': [
+                {"name": "Static data 1", "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 1"},
+                {"name": "Static data 2", "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 2"},
+                {"name": "Static data 3", "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 3"}
+            ],
+        })
+        return HttpResponse(template.render(context))
