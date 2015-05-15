de1d31e6c97507eef4ca402ce0cd24adc688fa48
Kim T <kim@bynd.com>
2015-05-15 17:09:35 -0400

    AngularJS adjustment

--- pages/static/angular.js
+++ pages/static/angular.js
@@ -8,8 +8,20 @@
 
 var app = angular.module('app', [])
 
-    .controller('dynamic', function ($scope, $http, $templateCache) {
-        $http.get(window.location.href).success(function (data) {
-            $scope.items = data.items;
+    /**
+     * @method template
+     */
+    .directive('template', function ($http, $templateCache, $compile) {
+        return ({
+            restrict: 'A',
+            link: function (scope, el, attrs) {
+                window.setTimeout(function () {
+                    $http.get(window.location.href).success(function (data) {
+                        scope.items = data.items;
+                        el.html($templateCache.get(attrs.template));
+                        $compile(el.contents())(scope);
+                    });
+                }, 1000);
+            }
         });
     });
